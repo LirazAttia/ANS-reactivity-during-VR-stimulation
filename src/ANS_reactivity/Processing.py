f684f416800c95409b78c163d4ae5a41d9a5b4b3
Keren Ruzal <kerenruzal@gmail.com>
2021-07-01 10:36:06 +0300

    changed weights to dict + added changing_weights method + modified tests

--- src/ANS_reactivity/Processing.py
+++ src/ANS_reactivity/Processing.py
@@ -8,16 +8,17 @@ import heartpy as hp
 from pandas.core.frame import DataFrame
 pd.options.mode.use_inf_as_na = True
 
-BAD_TYPE_MESSAGE = "Invalid input: ({value})! Only pathlib.Path and strings are accepted as data_path."
+BAD_PATH_TYPE_MESSAGE = "Invalid input: ({value})! Only pathlib.Path and strings are accepted as data_path."
 DIRECTORY_NOT_EXISTING_MESSAGE = "Invalide input: ({value})! Directory doesn't exist."
-HAVE_TO_BE_TUPLE_MESSAGE = "Invalid input: ({value})! Have to be tuple"
-TUPLE_LENGTH_MESSAGE  = "Invalid input: Length mismatch: lenght of tuple must be 3 (not {value})."
-TUPLE_SUM_MESSAGE = "Invalid input: The sum of the values must be 1 (not {value})."
-TUPLE_NEGATIVE_MESSAGE = "Invalid input: ({value})! tuple values must be positive."
-VALUES_ARE_NOT_NUMBERS_MESSAGE = "Invalid input: ({value})! tuple values must be type int or float."
+WEIGHTS_LENGTH_MESSAGE = "Invalid input: ({value}). Lenght of dict must be 3."
+BAD_KEYS_NAMES_MESSAGE = "Invalid input: ({value}). Weights' keys must be ['ECG', 'GSR', RESP']."
+WEIGHTS_SUM_MESSAGE = "Invalid input: The sum of the values must be 1 (not {value})."
+WEIGHTS_NEGATIVE_MESSAGE = "Invalid input: ({value})! weights values must be positive."
+WEIGHTS_ARE_NOT_NUMBERS_MESSAGE = "Invalid input: ({value})! tuple values must be type int or float."
 BAD_SAMPLE_RATE_TYPE_MESSAGE = "Invalid input: ({value})! Only ints are accepted as sample_rate."
 BAD_TIME_WINDOW_TYPE_MESSAGE = "Invalid input: ({value})! Only ints are accepted as time_window."
-BAD_WEIGHTS_TYPE_MESSAGE = "Invalid input: ({value})! Only tuples( , , ) are accepted as weights."
+BAD_WEIGHTS_TYPE_MESSAGE = "Invalid input: ({value})! Only dicts are accepted as weights."
+
 
 class OfflineAnalysisANS:
     """
@@ -27,38 +28,43 @@ class OfflineAnalysisANS:
     conforms with the larger data processing pipeline of this project.
     """
 
-    def __init__(self, data_path: str = r"ANS-reactivity-during-VR-stimulation\Data.csv", sample_rate: int = 512, time_window: int = 10, weights: tuple = (0.333, 0.333, 0.333)):
-      
-        pathlib_input = isinstance(data_path, Path)
-        str_input = isinstance(data_path, str)
-        if not (pathlib_input or str_input):
+    def __init__(self, data_path: str = r"ANS-reactivity-during-VR-stimulation\Data.csv", sample_rate: int = 512, time_window: int = 10, weights: dict = {'ECG': 1/3, 'GSR': 1/3, 'RESP': 1/3}):
+
+        if not (isinstance(data_path, Path) or isinstance(data_path, str)):
             raise TypeError(BAD_PATH_TYPE_MESSAGE.format(value=data_path))
         elif not Path(data_path).exists():
             raise ValueError(
                 DIRECTORY_NOT_EXISTING_MESSAGE.format(value=data_path))
         else:
             self.data_path = data_path
-        
-        sample_rate_int = isinstance(sample_rate, int)
-        if not sample_rate_int:
+
+        if not isinstance(sample_rate, int):
             raise TypeError(BAD_SAMPLE_RATE_TYPE_MESSAGE.format(value=sample_rate))
         else:
             self.sample_rate = sample_rate
-        
-        time_window_int = isinstance(time_window, int)
-        if not time_window_int:
+
+        if not isinstance(time_window, int):
             raise TypeError(BAD_TIME_WINDOW_TYPE_MESSAGE.format(value=sample_rate))
         else:
             self.time_window = time_window
 
-        weights_tuple = isinstance( weights, tuple)
-        if not weights_tuple:
+        if not isinstance(weights, dict):
             raise TypeError(BAD_WEIGHTS_TYPE_MESSAGE.format(value=weights))
+        elif len(weights) != 3:
+            raise ValueError(WEIGHTS_LENGTH_MESSAGE. format(value=weights))
+        elif list(weights.keys()) != ["ECG", "GSR", "RESP"]:
+            raise ValueError(BAD_KEYS_NAMES_MESSAGE.format(value=list(weights.keys())))
+        elif not (isinstance(weights["ECG"], (int, float)) and isinstance(weights["GSR"], (int, float)) and isinstance(weights["RESP"], (int, float))):
+            raise TypeError(WEIGHTS_ARE_NOT_NUMBERS_MESSAGE.format(value=weights))
+        elif sum(weights.values()) != 1:
+            raise ValueError(WEIGHTS_SUM_MESSAGE.format(value=sum(weights.values())))
+        elif not (weights["ECG"] >= 0 and weights["GSR"] >= 0 and weights["RESP"] >= 0):
+            raise ValueError(WEIGHTS_NEGATIVE_MESSAGE.format(value=weights))
         else:
             self.weights = weights
 
         self.n_samples = self.time_window*self.sample_rate
-        
+
     def read_data(self) -> DataFrame:
         """ Pulling and reading the data into Dataframe.
         parm:
@@ -66,9 +72,24 @@ class OfflineAnalysisANS:
         """
         self.raw_data = pd.read_csv(self.data_path)
 
-    
+    def change_weights(self, weights: dict = {'ECG': 1/3, 'GSR': 1/3, 'RESP': 1/3}):
+        if not isinstance(weights, dict):
+            raise TypeError(BAD_WEIGHTS_TYPE_MESSAGE.format(value=weights))
+        elif len(weights) != 3:
+            raise ValueError(WEIGHTS_LENGTH_MESSAGE. format(value=weights))
+        elif list(weights.keys()) != ["ECG", "GSR", "RESP"]:
+            raise ValueError(BAD_KEYS_NAMES_MESSAGE.format(value=list(weights.keys())))
+        elif not (isinstance(weights["ECG"], (int, float)) and isinstance(weights["GSR"], (int, float)) and isinstance(weights["RESP"], (int, float))):
+            raise TypeError(WEIGHTS_ARE_NOT_NUMBERS_MESSAGE.format(value=weights))
+        elif sum(weights.values()) != 1:
+            raise ValueError(WEIGHTS_SUM_MESSAGE.format(value=sum(weights.values())))
+        elif not (weights["ECG"] >= 0 and weights["GSR"] >= 0 and weights["RESP"] >= 0):
+            raise ValueError(WEIGHTS_NEGATIVE_MESSAGE.format(value=weights))
+        else:
+            self.weights = weights
+
     def heart_rate(self):
-        #Extracts heart rate from raw ECG data
+        # Extracts heart rate from raw ECG data
 
         number_of_chunks = (len(self.ecg))//self.n_samples
         heart_rate_for_every_chunk = np.zeros(number_of_chunks)
@@ -104,7 +125,7 @@ class OfflineAnalysisANS:
 
         self.processed_data = pd.DataFrame(
             columns=["TIME", "ECG", "RESP", "GSR"])
-       
+
         self.processed_data["TIME"] = self.time.iloc[0:-1:self.n_samples] #Not sure this is correct, the basic idea is marking each "time-frame" according to start-time
         self.processed_data["ECG"] = self.heart_rate()
         self.processed_data["RESP"] = self.resp_rate()
@@ -121,24 +142,12 @@ class OfflineAnalysisANS:
             self.processed_data[column] = (self.processed_data[column]-min)/max
         self.normal_data = self.processed_data.copy()
 
-
-    def score_adding(self, wights: tuple = (0.333, 0.333, 0.334)) -> DataFrame:
+    def score_adding(self) -> DataFrame:
         """ making an index according to wights.
         parm:
         return:
         """
-        if not isinstance(wights, tuple):
-            raise TypeError(HAVE_TO_BE_TUPLE_MESSAGE.format(value=wights))
-        elif len(wights) != 3:
-            raise ValueError(TUPLE_LENGTH_MESSAGE.format(value=len(wights)))
-        elif not (isinstance(wights[0], (int, float)) and isinstance(wights[1], (int, float)) and isinstance(wights[2], (int, float))):
-            raise TypeError(VALUES_ARE_NOT_NUMBERS_MESSAGE.format(value=wights))
-        elif sum(list(wights)) != 1:
-            raise ValueError(TUPLE_SUM_MESSAGE.format(value=sum(list(wights))))
-        elif not (wights[0] >=0 and wights[1] >=0 and wights[2] >=0):
-            raise ValueError(TUPLE_NEGATIVE_MESSAGE.format(value=wights))
-        else:
-            self.normal_data["Fear_Index"] = self.normal_data["ECG"]*wights[0] + self.normal_data["GSR"]*wights[1] + self.normal_data["RESP"]*wights[2]
-            self.scored_data = self.normal_data.copy()
+        self.normal_data["Stress_Score"] = self.normal_data["ECG"]*self.weights["ECG"] + self.normal_data["GSR"]*self.weights["GSR"] + self.normal_data["RESP"]*self.weights["RESP"]
+        self.scored_data = self.normal_data.copy()
 
-    #def_plot_stress_score()
+    # def plot_stress_score():
